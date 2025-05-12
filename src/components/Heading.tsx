4a84515bc1b9179f40ac9ca0e2503fd1e28fd226
Rayan Morais <rayancmorais@MacBook-Air-de-Rayan.local>
2025-05-12 16:44:17 -0300

    novo commit

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,4 @@
 import styles from './Heading.module.css';
 export function Heading() {
-  return <h1 className={styles.heading}>Olá Mundo!</h1>;
+  return <h1 className={styles.cyan}>Olá Mundo!</h1>;
 }
