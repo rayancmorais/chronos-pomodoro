d6109d1e842f0bbd0e41db09cf48acedb0dd668c
Rayan Morais <rayan_cm2021@icloud.com>
2025-05-12 19:18:12 -0300

    novo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import {Heading} from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
-import {TimerIcon} from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá world 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
 
-      <p>
-        Duis quis sit nostrud eu eu ad aute cupidatat sunt sit voluptate duis.
-        Nostrud commodo dolor duis est. Laborum aliqua ut veniam et.
-        Non eu est aliqua nostrud in esse dolore ex amet. 
-        Aute esse consequat est adipisicing amet sit laborum commodo eiusmod
-        eu consequat ut laborum quis.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
