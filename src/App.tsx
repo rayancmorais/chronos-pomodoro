b23b182cc07ae0f6c72be9afba8bdc6a8a577d19
Rayan Morais <rayan_cm2021@icloud.com>
2025-05-13 22:03:16 -0300

    reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Container} from './components/Container';
-import { CountDown } from './components/CountDown';
+import {CountDown} from './components/CountDown';
 import {Logo} from './components/Logo';
 import {Menu} from './components/Menu';
 
@@ -18,7 +18,29 @@ export function App() {
       </Container>
 
       <Container>
-        <CountDown/>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
