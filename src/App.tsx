d38f9a3380e1470c6b3021ee39ce013a2b4559e0
Rayan Morais <rayan_cm2021@icloud.com>
2025-05-27 17:05:25 -0300

    cria pag AboutPomodoro e NotFound com GenericHtml

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,7 @@
-import {Container} from './components/Container';
-import {CountDown} from './components/CountDown';
-import {Logo} from './components/Logo';
-import {Menu} from './components/Menu';
-import {DefaultInput} from './components/DefaultInput';
-
-import './styles/global.css';
 import './styles/theme.css';
-import {Cycles} from './components/Cycles';
-import {PlayCircleIcon, StopCircleIcon} from 'lucide-react';
-import {DefaultButton} from './components/DefaultButton';
+import './styles/global.css';
+import {Home} from './pages/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-          
-          <div className='formRow'>
-            <button>Enviar</button>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,90 @@
-import {HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
+import {useState, useEffect} from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); // Não segue o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+
+    // document.documentElement.setAttribute('data-theme', theme);
+  }
+  // useEffect(() => {
+  //   console.log('useEffect sem dependências', Date.now());
+  // }); // Executado todas vez que o componente renderiza na tela
+
+  // useEffect(() => {
+  //   console.log('useEffect com array deps vazio', Date.now());
+  // }, []); // Executa apenas quando o React monta o componente na tela pela primeira vez
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]); // Executa apenas quando o valor de theme muda
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <SunIcon />
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
 }
-
-//   /* command+f */
